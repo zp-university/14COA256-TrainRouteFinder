470bcfa1156f6056de940f4d09dcf27d5a4620d0
Zack <zackpollard@ymail.com>
2015-05-17 19:33:07 +0100

    Created TrainBookingCLI in preparation to start CLI programming soon.

---
+++ src/pro/zackpollard/trainbooking/cli/TrainBookingCLI.java
@@ -0,0 +1,17 @@
+package pro.zackpollard.trainbooking.cli;
+
+/**
+ * Created by Zack on 17/05/2015.
+ */
+public class TrainBookingCLI {
+
+    public static void main(String[] args) {
+
+        new TrainBookingCLI().run();
+    }
+
+    public void run() {
+
+        //TODO: Run stuff for the CLI.
+    }
+}
